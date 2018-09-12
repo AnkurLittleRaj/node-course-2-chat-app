9613b056b83ae5dbe138e5631bfb5bd435acebc6
unknown <raj.ankur8@gmail.com>
2018-09-12 10:45:17 +0530

    Add new Message and create message event

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+  var socket =io();
+    socket.on('connect',function () {
+        console.log("connected to server");
+
+        socket.emit('createMessage', {
+       from:"nscjnjdsjn@kk",
+       text:"hi there my name is dug"
+
+        })
+    });
+     socket.on('disconnect',function (){
+        console.log("Disconnected to server");
+    });
+    socket.on('newMessage',function (message){
+        console.log("New message",message);
+    });
--- server/server.js
+++ server/server.js
@@ -10,9 +10,16 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 console.log('new user connected');
+socket.emit('newMessage',{
+   "from":"ankur@gmail.com","text":"ankur is most awesome guy",
+   "createAt":123 
+});
  socket.on('disconnect',()=>{
         console.log("User was dissconected");
     });
+    socket.on('createMessage',(newEmail)=>{
+console.log('createdMessage',newEmail);
+    })
 })
 server .listen(port,()=>{
     console.log(`port is working on ${port}`);
